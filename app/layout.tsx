19add567dda706ca0915080f98900ed78d8cca07
YoeCK-sys <114443942+YoeCK-sys@users.noreply.github.com>
2024-05-07 10:31:08 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mundo Militar",
-  description: "...",
+  title: "Yoel Dev",
+  description: "PORTAFOLIO EN DESARROLLO",
   icons: "@"
 };
 
